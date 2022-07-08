60e12abd9a3110f17ead9aba06fd842fc6a95838
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-07-08 18:06:13 +0530

    3.1.1

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 setup(name="Armstrongnumberfinder",
-version="3.1.0",description="This package allows you to find armstrong number",
+version="3.1.1",description="This package allows you to find armstrong number",
   long_description=long_description,
     long_description_content_type='text/markdown',
 author="Shourjjo Majumder",
 author_email="contact@shourgamer2.tk",
-packages=['armstrongnbrfinder'],requires=['requests'])
+packages=['armstrongnbrfinder'])
