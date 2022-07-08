137d35033f560fa89ab6c72bd9280abca32cb6a6
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-07-08 18:06:41 +0530

    Update __init__.py

--- armstrongnbrfinder/__init__.py
+++ armstrongnbrfinder/__init__.py
@@ -1,7 +1,15 @@
-import requests
-import os
-from requests import get
-def armstrongnbr(number):
-    api = (f"https://armstrong-nbr-finder.herokuapp.com/armstrong/{number}")
-    text = get(api).text
-    print(text)
+
+def armstrongnbr(n):
+    sum = 0
+    order = len(str(n))
+    copy_n = n
+    while(n>0):
+        digit = n%10
+        sum += digit **order
+        n = n//10
+    if(sum == copy_n):
+        print(f"{copy_n} is an armstrong number")
+        return "True"
+    else:
+        print(f"{copy_n} is not an armstrong number")
+        return "False"
